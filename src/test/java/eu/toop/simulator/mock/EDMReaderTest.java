91d0dbeaa952bd671a2b385ac1373ea28d4b49af
Muhammet Yildiz <myildiz83@gmail.com>
2020-05-24 02:26:37 +0300

    fixed edm reading bug

---
+++ src/test/java/eu/toop/simulator/mock/EDMReaderTest.java
@@ -0,0 +1,19 @@
+package eu.toop.simulator.mock;
+
+import eu.toop.edm.IEDMTopLevelObject;
+import eu.toop.edm.xml.EDMPayloadDeterminator;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EDMReaderTest {
+
+  @Test
+  public void readQueryRequest() throws IOException {
+    try (final InputStream inputStream = this.getClass().getResourceAsStream("/sample_query_request.xml")) {
+      final IEDMTopLevelObject aTopLevel = EDMPayloadDeterminator.parseAndFind(inputStream);
+    }
+  }
+}
